d3b1977dce63090c0f3351ea0dbb4b2a0643538c
ayes <ayes.email@gmail.com>
2014-06-13 05:23:10 +0800

    bsm changes

--- mysmbp/urls.py
+++ mysmbp/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('smbp.urls')),
+    url(r'^media/(.*)$', 'django.views.static.serve', {'document_root' : settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
 )
